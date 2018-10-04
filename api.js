ad992e3ea2fea4e0e6c3aef69d70193b2ca9baf9
jonatm15 <31699593+jonatm15@users.noreply.github.com>
2018-10-04 13:57:23 +0200

    Add files via upload

---
+++ api.js
@@ -0,0 +1,7 @@
+/*
+Create user:
+METHOD: POST
+ENDPOINT:api/user
+BODY (json): {"name":"Jonas Magnussen", "email":"jonasmagnussen@gmail.com", "password":"1234343"}
+RESPONS (json): {"id":123456"name":"Jonas Magnussen", "email":"jonasmagnussen@gmail.com", "password":"1234343"}
+*/
